ff006a756d4b07e378c49da76f4c2c0d57e3d706
owening <ouyang5507@foxmail.com>
2023-08-03 20:50:17 +0800

    selenium L1 work commit

--- selenium_demo/locator_demo.py
+++ selenium_demo/locator_demo.py
@@ -7,24 +7,32 @@
 @Author ：Owen
 @Date ：2023/8/2 10:09 
 '''
-from time import sleep
+import json
+import time
+
+
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
 driver = webdriver.Chrome()
 driver.get("https://vip.ceshiren.com/#/ui_study")
-driver.implicitly_wait(3)
+# driver.implicitly_wait(3)
 driver.maximize_window()
-driver.find_element(By.LINK_TEXT,"元素定位").click()
+# str = driver.find_element(By.CSS_SELECTOR,"#locate > span").text
+# driver.find_element(By.CSS_SELECTOR,"#locate > span").click()
+# driver.find_element(By.XPATH,"//span[normalize-space()='father']")
 
-driver.find_element(By.CSS_SELECTOR,'#ember26 > a').click()
-# str1 = dr.text
-# dr.click()
-sleep(1)
-driver.find_element(By.XPATH,'//*[@id="ember102"]/a').click()
-sleep(1)
-driver.back()
+WebDriverWait(driver, 10).until(expected_conditions.element_to_be_clickable((By.CSS_SELECTOR, "#success_btn")))
+driver.find_element(By.CSS_SELECTOR, "#success_btn > span").click()
 
+# WebDriverWait(driver, 10).until(expected_conditions.element_to_be_clickable((By.CSS_SELECTOR, '#success_btn')))
+# driver.find_element(By.CSS_SELECTOR, "#success_btn").click()
+# str2 = driver.find_element(By.CSS_SELECTOR,"#success_btn > span").text
+
+# print(str2)
+time.sleep(3)
 
 driver.quit()
---
+++ selenium_demo/test_ceshiren.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''
+@Project ：hogwarts 
+@File ：test_ceshiren
+@IDE  ：PyCharm 
+@Author ：Owen
+@Date ：2023/8/3 19:29 
+'''
+import time
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class TestCeShiRen:
+
+    def setup(self):
+        """
+        前置步骤
+        """
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(3)
+        self.driver.maximize_window()
+        self.driver.get("https://ceshiren.com/search?expanded=true")
+
+
+    def teardown(self):
+        """
+        后置步骤
+        """
+        self.driver.quit()
+
+    @pytest.mark.parametrize("keyword",["selenium","appium","面试"])
+    def test_search(self,keyword):
+        """
+        测试步骤
+        断言
+        """
+        self.driver.find_element(By.CSS_SELECTOR,"[placeholder='搜索']").send_keys(keyword)
+        self.driver.find_element(By.CSS_SELECTOR,".search-cta").click()
+        res_element = self.driver.find_element(By.CSS_SELECTOR,".topic-title")
+        assert keyword in res_element.text.lower()
+
+
+
+
