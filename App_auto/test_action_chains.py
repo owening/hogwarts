d976cf42ec309cd8c96a2c0baf90e72de582cbaa
owening <ouyang5507@foxmail.com>
2023-08-23 18:11:52 +0800

    手机密码滑动解锁

--- App_auto/test_action_chains.py
+++ App_auto/test_action_chains.py
@@ -7,8 +7,10 @@
 @Author ：Owen
 @Date ：2023/8/23 15:06 
 '''
+import logging
 import time
 
+import allure
 from appium import webdriver
 from appium.webdriver.common.appiumby import AppiumBy
 from selenium.webdriver import ActionChains
@@ -20,36 +22,28 @@ from selenium.webdriver.common.actions.pointer_input import PointerInput
 class TestAction:
 
     def setup(self):
-        # desired_caps = {}
-        # desired_caps['automationName'] = 'UiAutomator2'
-        # desired_caps['platformName'] = 'Android'
-        # desired_caps['platformVersion'] = '12.0'
-        # desired_caps['deviceName'] = 'emulator-5554'
-        # desired_caps['appPackage'] = 'cn.kmob.screenfingermovelock'
-        # desired_caps['appActivity'] = 'com.samsung.ui.FlashActivity'
-        # desired_caps['noReset'] = "true"
-        # desired_caps['deviceReadyTimeout'] = 10
+        desired_caps = {}
+        desired_caps['automationName'] = 'UiAutomator2'
+        desired_caps['platformName'] = 'Android'
+        desired_caps['platformVersion'] = '6.0'
+        desired_caps['deviceName'] = 'emulator-5554'
+        desired_caps['appPackage'] = 'cn.kmob.screenfingermovelock'
+        desired_caps['appActivity'] = 'com.samsung.ui.FlashActivity'
+        desired_caps['noReset'] = "true"
+        desired_caps['deviceReadyTimeout'] = 10
 
-        desired_caps = {
-            "platformName": "Android",
-            "appium:appPackage": "cn.kmob.screenfingermovelock",
-            "appium:appActivity": "com.samsung.ui.FlashActivity",
-            "appium:automationName": "UiAutomator2",
-            "appium:noReset": True
-        }
         self.driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", desired_caps)
         self.driver.implicitly_wait(10)
 
     def teardown(self):
-        print("测试退出app")
         self.driver.quit()
 
+    @allure.title("滑动解锁案例")
     def test_action_chains(self):
-        print("滑动解锁案例")
         self.driver.find_element(AppiumBy.XPATH, "//*[@text='设置手势']").click()
 
         actions = ActionChains(self.driver)
-        actions.w3c_actions = ActionBuilder(self.driver, mouse=PointerInput(interaction.POINTER_TOUCH,"touch"))
+        actions.w3c_actions = ActionBuilder(self.driver, mouse=PointerInput(interaction.POINTER_TOUCH, "touch"))
         actions.w3c_actions.pointer_action.move_to_location(122, 199)
         actions.w3c_actions.pointer_action.pointer_down()
         actions.w3c_actions.pointer_action.move_to_location(363, 404)
